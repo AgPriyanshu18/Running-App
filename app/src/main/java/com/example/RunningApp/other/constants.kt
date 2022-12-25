f6dbdabaa636bde5c4d63bfc1d2a784743b1372d
Priyanshu <agpriyanshu18@gmail.com>
2022-12-25 18:19:42 +0530

    Setting Up Permissions DONE

--- app/src/main/java/com/example/RunningApp/other/constants.kt
+++ app/src/main/java/com/example/RunningApp/other/constants.kt
@@ -4,4 +4,5 @@ import com.example.RunningApp.DB.RunningDatabase
 
 object constants {
      const val RUNNING_DATABASE_NAME = "running_db"
+     const val REQUEST_CODE_LOCATION_PERMISSION = 1
 }
--- app/src/main/java/com/example/RunningApp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/RunningApp/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.example.RunningApp.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +11,15 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.RunningApp.R
 import com.example.RunningApp.databinding.FragmentRunBinding
+import com.example.RunningApp.other.TrackingUtility
+import com.example.RunningApp.other.constants.REQUEST_CODE_LOCATION_PERMISSION
 import com.example.RunningApp.ui.viewmodels.MainViewModal
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment : Fragment() {
+class RunFragment : Fragment() , EasyPermissions.PermissionCallbacks{
     private val viewModal : MainViewModal by viewModels()
 
     private var binding : FragmentRunBinding ?= null
@@ -25,11 +31,58 @@ class RunFragment : Fragment() {
     ): View? {
 
         binding = FragmentRunBinding.inflate(layoutInflater)
-
+        requestPermission()
         binding?.fab?.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
 
         return binding?.root
+    }
+
+    private fun requestPermission(){
+        if(TrackingUtility.hasLocationPermission(requireContext())){
+            return
+        }
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location Permission",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        }else{
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location Permission",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            AppSettingsDialog.Builder(this).build().show()
+        }else{
+            requestPermission()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this)
     }
 }
