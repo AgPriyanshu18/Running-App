d906c84af9ca0e87fecf98027dc890ccbeec3d41
Priyanshu <agpriyanshu18@gmail.com>
2022-12-25 12:57:00 +0530

    Basic Dagger Hilt Prep done

---
+++ app/src/main/java/com/example/RunningApp/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.example.RunningApp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+}
---
+++ app/src/main/java/com/example/RunningApp/other/constants.kt
@@ -0,0 +1,7 @@
+package com.example.RunningApp.other
+
+import com.example.RunningApp.DB.RunningDatabase
+
+object constants {
+     const val RUNNING_DATABASE_NAME = "running_db"
+}
